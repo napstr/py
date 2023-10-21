f903d7ea0a64f505d4e70259ac5132351ef26db3
War On Stupidity <88652564+WarOnStupidity@users.noreply.github.com>
2023-10-22 03:12:05 +0530

    Create udp-client.py

---
+++ tcp/udp-client.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import socket
+
+# UDP Client
+
+target_host = "127.0.0.1"
+target_port = 80
+
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# send some data
+sdata = "AAABBBCCC"
+client.sendto(sdata.encode("utf-8"),(target_host,target_port))
+
+# receive some data
+data, addr = client.recvfrom(4096).decode("utf-8")
+print (data)
