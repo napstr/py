d137625e45b609a859361f9be6e51476e68fcf33
War On Stupidity <88652564+WarOnStupidity@users.noreply.github.com>
2023-10-22 03:14:05 +0530

    Create cgi-server.py

---
+++ http/cgi-server.py
@@ -0,0 +1,36 @@
+import os
+import cgi
+import argparse
+
+import http.server
+# Comment out the above line and uncomment the below for Python 2.7.x.
+#import BaseHTTPServer
+
+# Uncomment the below line for Python 2.7.x.
+#import CGIHTTPServer
+
+import cgitb 
+cgitb.enable()  ## enable CGI error reporting
+
+
+def web_server(port):
+
+    server = http.server.HTTPServer
+    # Comment out the above line and uncomment the below for Python 2.7.x.
+    #server = BaseHTTPServer.HTTPServer
+
+    handler = http.server.CGIHTTPRequestHandler #RequestsHandler
+    # Comment out the above line and uncomment the below for Python 2.7.x.
+    #handler = CGIHTTPServer.CGIHTTPRequestHandler #RequestsHandler
+
+    server_address = ("", port)
+    handler.cgi_directories = ["/cgi-bin", ]
+    httpd = server(server_address, handler)
+    print ("Starting web server with CGI support on port: %s ..." %port)
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='CGI Server Example')
+    parser.add_argument('--port', action="store", dest="port", type=int, required=True)
+    given_args = parser.parse_args()
+    web_server(given_args.port)
